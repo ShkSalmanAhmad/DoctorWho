d601d9387edcdb1f9884ba74f9687f80534e423e
Sheikh Salman Ahmad <47979823+ShkSalmanAhmad@users.noreply.github.com>
2019-09-29 13:34:07 +0500

    Add files via upload

---
+++ Lab5a/Lab5a/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lab5a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.flowbar = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ColorsPallette = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.flowbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(59, 22);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Colour";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(228, 22);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // flowbar
+            // 
+            this.flowbar.Location = new System.Drawing.Point(59, 74);
+            this.flowbar.Maximum = 5;
+            this.flowbar.Name = "flowbar";
+            this.flowbar.Size = new System.Drawing.Size(96, 45);
+            this.flowbar.TabIndex = 1;
+            this.flowbar.Scroll += new System.EventHandler(this.flowbar_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(59, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 433);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowbar);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.flowbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TrackBar flowbar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog ColorsPallette;
+    }
+}
+
---
+++ Lab5a/Lab5a/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Lab5a
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private SolidBrush b;
+        private Color c = Color.Black;
+        int CurrentValue;
+        int PreviousValue;
+        private Pen p;
+        public Form1()
+        {
+            InitializeComponent();
+            CurrentValue = 0;
+            PreviousValue = 0;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorsPallette.Color = c;
+            ColorsPallette.ShowDialog();
+            c = ColorsPallette.Color;
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            p = new Pen(c);
+            b = new SolidBrush(c);
+
+            g = this.CreateGraphics();
+
+            g.DrawLine(p, 120, 250, 120, 400); //draw the left vertical line
+            g.DrawLine(p, 120, 400, 300, 400); //draw the horizontal bottom line
+            g.DrawLine(p, 300, 250, 300, 400); //draw the right vertical line
+        }
+
+        private void flowbar_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+            if (CurrentValue == 0)
+            {
+                CurrentValue = flowbar.Value;
+            }
+            else
+            {
+                PreviousValue = CurrentValue;
+                CurrentValue = flowbar.Value;
+            }
+            for (double i = 400; i >= 250; i = i - (CurrentValue * 0.1))
+            {
+                g.FillRectangle(b, new Rectangle(143, Convert.ToInt32(150), 30, Convert.ToInt32(250)));
+                g.FillRectangle(b, new Rectangle(120, Convert.ToInt32(i), 180, Convert.ToInt32(400 - i)));
+                Thread.Sleep(1);
+            }
+            flowbar.Value = 0;
+        }
+
+
+    }
+}
